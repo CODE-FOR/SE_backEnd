6ac8da8a66ea22db1c75cf48a825b910a6269723
liu <meetliuzhenwei@gmail.com>
2021-05-20 19:26:30 +0800

    add receive send msg

--- core/consumers.py
+++ core/consumers.py
@@ -12,13 +12,21 @@ class chatConsumer(WebsocketConsumer):
     def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
         # message  =text_data_json['message']
+        code = text_data_json['code']
+        print(code)
+        print(text_data_json)
+        if code == 600:
+            self.deal_send_msg(text_data_json['message'])
 
-        self.send(text_data=json.dumps({
-            'message': 'webSocket!'
-        }))
+    def deal_send_msg(self, get_msg):
 
-        sleep(10)
-
-        self.send(text_data=json.dumps({
-            'message': 'webSocket!'
-        }))
+        msg = {}
+        msg['code'] = 610
+        msg['sender_id'] = 3
+        msg['receiver_id'] = 2
+        msg['msg'] = {
+            'time': '2021-05-09',
+            'send_id': 3,
+            'message': get_msg
+        }
+        self.send(text_data=json.dumps(msg))
