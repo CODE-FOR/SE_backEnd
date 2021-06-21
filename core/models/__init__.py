d3a3480997261bfdc36608be91137a87be7a9111
kumolv(吕云帆) <kumolv@tencent.com>
2021-06-21 12:08:54 +0800

    merge

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -6,6 +6,5 @@ from .tag import Tag
 from .message import Message
 from .comment import Comment
 from .auth_record import AuthRecord
-from .notification import Notification
 
 from .image import Image
